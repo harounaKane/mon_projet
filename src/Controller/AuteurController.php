2535bfc3f42cecb57b3b529137b8b72e8536de30
harounaKane <harouna.kane@hotmail.fr>
2023-02-14 17:04:51 +0100

    entity (auteur)

--- src/Controller/AuteurController.php
+++ src/Controller/AuteurController.php
@@ -2,18 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Auteur;
+use App\Repository\AuteurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class AuteurController extends AbstractController
 {
     #[Route('/auteur', name: 'app_auteur')]
-    public function index(): Response
-    {
-        $auteurs = [];
+    public function index(AuteurRepository $auteurRepo): Response
+    {     
         return $this->render('auteur/index.html.twig', [
-            "auteurs" => $auteurs
+            "auteurs" => $auteurRepo->findAll()
         ]);
+    }
+
+    #[Route('/auteur/{id}', name: "app_auteur_show")]
+    public function show(Auteur $auteur){
+        return $this->render("auteur/show.html.twig", ["auteur" => $auteur]);
+    }
+
+    /**
+     * @Route("/auteur/new", name="app_auteur_new")
+     */
+    public function new(){
+
+        dump(555);
+        return $this->render("auteur/new.html.twig");
+
+      //  return $this->redirectToRoute('app_auteur');
+    }
+
+    #[Route('/delete/{id}', name: "app_auteur_delete")]
+    public function delete(Auteur $auteur, AuteurRepository $repo){
+        $repo->remove($auteur, true);
+        return $this->redirectToRoute('app_auteur');
     }
 }
---
+++ src/Entity/Auteur.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuteurRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuteurRepository::class)]
+class Auteur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_ajout = null;
+
+    #[ORM\Column]
+    private ?bool $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateAjout(): ?\DateTimeInterface
+    {
+        return $this->date_ajout;
+    }
+
+    public function setDateAjout(\DateTimeInterface $date_ajout): self
+    {
+        $this->date_ajout = $date_ajout;
+
+        return $this;
+    }
+
+    public function isStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
